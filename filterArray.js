65e9fd43931781cae28dfc82811fa758dd2a285e
Kay Holten <holtenkay@gmail.com>
2021-01-24 15:03:26 -0800

    time complexity added

--- filterArray.js
+++ filterArray.js
@@ -13,3 +13,10 @@ function noLessThanFive(array) {
 console.log(noLessThanFive([1, 4, 16, 2, 12, 22]))
 console.log(noLessThanFive([1, 2, 3, 4, 5, 6]))
 console.log(noLessThanFive([6, 7, 8, 9, 10]))
+
+/*
+
+Time-complexity: O(n). No, it cannot be optimized because no matter what,
+you'll need to look at every value in the array to make sure it's not 5 or less.
+
+*/
